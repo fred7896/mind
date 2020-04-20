554929c53aac41b0add315e4ae06c1b30687b38d
FredericMentel <frederic.mentel@gmail.com>
2020-04-20 10:17:03 +0200

    Count cards system wip.

--- frontend/src/Containers/Game.jsx
+++ frontend/src/Containers/Game.jsx
@@ -17,7 +17,10 @@ export default class Game extends React.Component {
 		cardsInPlayersHands: null,
 		players: [],
 		userId: "",
-		myDeck: []
+		myDeck: [],
+		allPlayersCountCards : [],
+		othersPlayersCountCards : []
+
 	};
 
 	componentDidMount() {
@@ -30,6 +33,9 @@ export default class Game extends React.Component {
 		if (this.state.cardgame.length > 0) {
 			this.setCardState();
 		}
+		if (this.state.players.length > 0) {
+			this.renderMyHand();
+		}
 		this.getCountCardHand();
 	}
 
@@ -37,6 +43,11 @@ export default class Game extends React.Component {
 		if (prevState.cardstate !== this.state.cardstate) {
 			this.getCountCardHand();
 			this.renderMyHand();
+			this.renderTeammatesDeck();
+		}
+		if (prevState.players !== this.state.players && this.state.players.length > 0) {
+			this.renderMyHand();
+			this.renderTeammatesDeck();
 		}
 	}
 
@@ -107,6 +118,7 @@ export default class Game extends React.Component {
 							data
 						)
 						.then(res => {
+							console.log("getplayers");
 							this.getGameInfos();
 							this.getPlayers();
 						});
@@ -240,31 +252,82 @@ export default class Game extends React.Component {
 	};
 
 	renderTeammatesDeck = () => {
-		const myPlayerInfo = this.state.players.filter(e => {
-			return e.id_user === this.state.userId;
-		});
-		console.log(myPlayerInfo);
+		const othersPlayers = this.state.players
+			.filter(e => {
+				return e.id_user !== this.state.userId;
+			})
+			.map(e => {
+				return { id_user_game: e.id_user_game, user_name: e.user_name };
+			});
+		//console.log(othersPlayers);
 		const cardsInPlayersHand = this.state.cardstate.filter(e => {
 			return e.id_slot == 2;
 		});
-		console.log(cardsInPlayersHand);
+		//console.log(cardsInPlayersHand);
+
+		// Count Cards by id_game_user
+		const result = [
+			...cardsInPlayersHand
+				.reduce((mp, o) => {
+					if (!mp.has(o.id_game_user)) mp.set(o.id_game_user, { ...o, count: 0 });
+					mp.get(o.id_game_user).count++;
+					return mp;
+				}, new Map())
+				.values()
+		].map(e => {
+			return {
+				id_user_game: e.id_game_user,
+				countCard: e.count
+			};
+		});
+
+		console.log(result);
+
+		const allPlayersCountCards = this.state.players.map(player => {
+			console.log(player);
+			let countTab = result.filter(count =>  { return player.id_user_game == count.id_user_game });
+			console.log(countTab);
+			return {id_user_game : player.id_user_game , user_name : player.user_name, countCard : countTab[0].countCard }
+		});
+
+		const othersPlayersCountCards = othersPlayers.map(player => {
+			console.log(player);
+			let countTab = result.filter(count =>  { return player.id_user_game == count.id_user_game });
+			console.log(countTab);
+			return {id_user_game : player.id_user_game , user_name : player.user_name, countCard : countTab[0].countCard }
+		});
+
+		this.setState({
+			allPlayersCountCards : allPlayersCountCards,
+			othersPlayersCountCards : othersPlayersCountCards
+		})
+
+		//console.log(allPlayersCountCards);
+		//console.log(othersPlayersCountCards);
+
 	};
 
 	renderMyHand = () => {
-		const cardInMyHand = this.state.cardstate.filter(e => {
-			return e.id_slot == 2 && e.id_game_user == 9;
+		let gameUser = this.state.players.filter(e => {
+			return e.id_user == this.state.userId;
 		});
-		console.log(cardInMyHand);
-		for (let i = 0; i < cardInMyHand.length; i++) {
-			let value = cardInMyHand[i].id_card_game;
-			axios.get(`http://localhost:4001/api/cardgame/value/${value}`).then(res => {
-				console.log(res.data[0].id_card);
-				if (!this.state.myDeck.includes(res.data[0].id_card)) {
-					this.setState({
-						myDeck: [...this.state.myDeck, res.data[0].id_card]
-					});
-				}
+		console.log(gameUser);
+		if (gameUser.length > 0) {
+			const cardInMyHand = this.state.cardstate.filter(e => {
+				return e.id_slot == 2 && e.id_game_user == gameUser[0].id_user_game;
 			});
+			console.log(cardInMyHand);
+			for (let i = 0; i < cardInMyHand.length; i++) {
+				let value = cardInMyHand[i].id_card_game;
+				axios.get(`http://localhost:4001/api/cardgame/value/${value}`).then(res => {
+					console.log(res.data[0].id_card);
+					if (!this.state.myDeck.includes(res.data[0].id_card)) {
+						this.setState({
+							myDeck: [...this.state.myDeck, res.data[0].id_card]
+						});
+					}
+				});
+			}
 		}
 	};
 
@@ -281,13 +344,13 @@ export default class Game extends React.Component {
 	};
 
 	render() {
-		//console.log(this.state.gameInfos);
-		//console.log(this.state.cardgame);
-		// console.log(this.state.cardstate);
+		// console.log(this.state.gameInfos);
+		// console.log(this.state.cardgame);
+		console.log(this.state.cardstate);
 		// console.log(this.state.cardsInPlayersHands);
 		// console.log(this.state.userId);
-		// console.log(this.state.players);
-		console.log(this.state.myDeck);
+		console.log(this.state.players);
+		// console.log(this.state.myDeck);
 		return (
 			<div className="container fontCyan boldLife game">
 				<div className="d-flex my-3">
@@ -323,23 +386,21 @@ export default class Game extends React.Component {
 					{this.state.cardsInPlayersHands === 0 && (
 						<div onClick={this.drawCards}>DrawCards</div>
 					)}
-					{/* <div>
-						{this.state.cardstate.map(e => {
-							return <div>klm</div>;
-						})}
-					</div> */}
 				</div>
 				<div className="d-flex my-5">
-					<div className="container-deck" onClick={this.renderTeammatesDeck}>
+				{this.state.othersPlayersCountCards.map(card => {
+					return (
+						<>
+						<div className="container-deck">
 						<SvgFactory iconname="deck" />
 
-						<div className="count-deck">2</div>
+						<div className="count-deck">{card.countCard}</div>
+						<div className="username">{card.user_name.toLowerCase()}</div>
 					</div>
-					<div className="container-deck" onClick={this.renderTeammatesDeck}>
-						<SvgFactory iconname="deck" />
-
-						<div className="count-deck">3</div>
-					</div>
+					
+					</>
+					)
+				})}
 				</div>
 
 				<div className="d-flex justify-content-center align-items-center mb-5">
@@ -351,7 +412,10 @@ export default class Game extends React.Component {
 					<div style={{ width: "60%" }} className="container-circle mx-2">
 						<SvgFactory iconname="circle" />
 
-						<div className="numero count-main">2</div>
+						{/* <div className="numero count-main">2</div> */}
+						<div className="numero">
+							<img src={require("../runner_start.svg")} width="100%" />
+						</div>
 					</div>
 					<div style={{ width: "20%" }} className="container-circle mustard mx-1">
 						<SvgFactory iconname="circle" />
@@ -364,7 +428,11 @@ export default class Game extends React.Component {
 				<div className="d-flex justify-content-center align-items-center">
 					{this.state.myDeck.map((e, idx) => {
 						return (
-							<div key={idx} style={{ width: "90px" }} className="container-card mx-1">
+							<div
+								key={idx}
+								style={{ width: "90px" }}
+								className="container-card mx-1"
+							>
 								<SvgFactory iconname="card" />
 
 								<div className="numero count-last">{e}</div>
